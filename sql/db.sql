27f9b0a52b192412ef7beb12bf19bd5cfc6afbb1
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-11-07 15:27:25 +0100

    ajout donnée

--- sql/db.sql
+++ sql/db.sql
@@ -2,36 +2,36 @@ SET foreign_key_checks = 0;
 
 DROP TABLE IF EXISTS Soiree;
 CREATE TABLE Soiree (
-    id  INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(30),
-    theme VARCHAR(30),
-    date DATE,
-    tarif DOUBLE(8,2),
-    id_lieu INT(4)
+                        id  INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                        nom VARCHAR(30),
+                        theme VARCHAR(30),
+                        date DATETIME,
+                        tarif DOUBLE(8,2),
+                        id_lieu INT(4)
 )ENGINE=INNODB;
 
 
 DROP TABLE IF EXISTS Lieu;
 CREATE TABLE Lieu (
-    id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(30),
-    adresse VARCHAR(30),
-    ville VARCHAR(30),
-    nbPLaceAssise INT(6),
-    nbPlaceDebout INT(6)
+                      id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                      nom VARCHAR(30),
+                      adresse VARCHAR(30),
+                      ville VARCHAR(30),
+                      nbPLaceAssise INT(6),
+                      nbPlaceDebout INT(6)
 )ENGINE=INNODB;
 
 
 DROP TABLE IF EXISTS Spectacle;
 CREATE TABLE Spectacle (
-    id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    titre VARCHAR(30),
-    description VARCHAR(30),
-    heure DATE,
-    duree VARCHAR(30),
-    libelleStyle VARCHAR(90),
-    video VARCHAR(90),
-    id_soiree INT(4)
+                           id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                           titre VARCHAR(30),
+                           description VARCHAR(30),
+                           heure DATETIME,
+                           duree VARCHAR(30),
+                           libelleStyle VARCHAR(90),
+                           video VARCHAR(200),
+                           id_soiree INT(4)
 )ENGINE=INNODB;
 
 
@@ -39,43 +39,43 @@ CREATE TABLE Spectacle (
 
 DROP TABLE IF EXISTS Artiste;
 CREATE TABLE Artiste (
-    id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Nom VARCHAR(30),
-    info VARCHAR(30)
+                         id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                         Nom VARCHAR(30),
+                         info VARCHAR(200)
 )ENGINE=INNODB;
 
 
 DROP TABLE IF EXISTS User;
 CREATE TABLE User (
-    id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(30),
-    passwd VARCHAR(30),
-    role  INT(4)
+                      id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                      email VARCHAR(30),
+                      passwd VARCHAR(200),
+                      role  INT(4)
 )ENGINE=INNODB;
 
 
 DROP TABLE IF EXISTS Image;
 CREATE TABLE Image (
-    id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    filetype VARCHAR(30),
-    description VARCHAR(50),
-    data MEDIUMBLOB
+                       id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                       filetype VARCHAR(200),
+                       description VARCHAR(200),
+                       data MEDIUMBLOB
 )ENGINE=INNODB;
 
 
 DROP TABLE IF EXISTS Spectacle2Artiste;
 CREATE TABLE Spectacle2Artiste (
-    id_spectacle INT(4),
-    id_artiste INT(4),
-    PRIMARY KEY (id_spectacle, id_artiste)
+                                   id_spectacle INT(4),
+                                   id_artiste INT(4),
+                                   PRIMARY KEY (id_spectacle, id_artiste)
 )ENGINE=INNODB;
 
 
 DROP TABLE IF EXISTS Spectacle2Image;
 CREATE TABLE Spectacle2Image (
-    id_spectacle INT(4),
-    id_image INT(4),
-    PRIMARY KEY (id_spectacle, id_image)
+                                 id_spectacle INT(4),
+                                 id_image INT(4),
+                                 PRIMARY KEY (id_spectacle, id_image)
 
 )ENGINE=INNODB;
 
--- sql/donnees.sql
+++ sql/donnees.sql
@@ -1,47 +1,60 @@
 -- Remplissage de la table Lieu
 INSERT INTO Lieu (nom, adresse, ville, nbPLaceAssise, nbPlaceDebout) VALUES
-                                                                         ('Théâtre National', '12 Rue de la Scène', 'Paris', 500, 200),
-                                                                         ('Salle du Spectacle', '22 Boulevard des Arts', 'Lyon', 300, 100),
-                                                                         ('Centre Culturel', '33 Avenue du Théâtre', 'Marseille', 400, 150);
+                                                                         ('Théâtre de Nancy', '1 Place Stanislas', 'Nancy', 500, 300),
+                                                                         ('Salle Poirel', '3 Rue Victor Poirel', 'Nancy', 400, 200),
+                                                                         ('Centre Culturel Jean Prouvé', '10 Rue de la Liberté', 'Nancy', 350, 150);
 
 -- Remplissage de la table Soiree
 INSERT INTO Soiree (nom, theme, date, tarif, id_lieu) VALUES
-                                                          ('Soirée Comédie', 'Comédie', '2024-12-01', 20.00, 1),
-                                                          ('Soirée Jazz', 'Jazz', '2024-12-05', 15.00, 2),
-                                                          ('Soirée Théâtre', 'Théâtre Classique', '2024-12-10', 25.00, 3);
+                                                          ('Soirée Rock Vintage', 'Rock Années 80', '2024-11-15 20:00:00', 18.00, 1),
+                                                          ('Soirée Punk', 'Punk Rock', '2024-11-17 21:30:00', 20.00, 2),
+                                                          ('Soirée Rock Alternatif', 'Alternatif', '2024-11-19 22:00:00', 22.00, 3);
 
 -- Remplissage de la table Spectacle
 INSERT INTO Spectacle (titre, description, heure, duree, libelleStyle, video, id_soiree) VALUES
-                                                                                             ('Comedy Night', 'Spectacle humoristique', '2024-12-01 20:00:00', '2h', 'Humour', 'comedy_night.mp4', 1),
-                                                                                             ('Jazz Live', 'Concert de jazz en live', '2024-12-05 21:00:00', '1h30', 'Jazz', 'jazz_live.mp4', 2),
-                                                                                             ('Théâtre Classique', 'Pièce de théâtre', '2024-12-10 19:30:00', '2h30', 'Classique', 'theatre_classique.mp4', 3);
+                                                                                             ('Rock Legends', 'Hommage aux légendes du rock', '2024-11-15 20:00:00', '3h', 'Rock', 'rock_legends.mp4', 1),
+                                                                                             ('Punk Vibes', 'Concert de punk rock explosif', '2024-11-17 23:30:00', '2h30', 'Punk', 'punk_vibes.mp4', 1),
+
+                                                                                             ('Punk Vibes', 'Concert de punk rock explosif', '2024-11-17 21:00:00', '2h30', 'Punk', 'punk_vibes.mp4', 2),
+                                                                                             ('Maxime&Alexandre', 'Soirée slow', '2024-11-19 00:00:00', '1h', 'Romantique', 'slow.mp4', 2),
+
+                                                                                             ('Indie Night', 'Soirée rock alternatif', '2024-11-19 20:00:00', '3h', 'Indie Rock', 'indie_night.mp4', 3);
 
 -- Remplissage de la table Artiste
 INSERT INTO Artiste (Nom, info) VALUES
-                                    ('Jean Dupont', 'Humoriste'),
-                                    ('Marie Leclerc', 'Chanteuse de jazz'),
-                                    ('Paul Durand', 'Acteur de théâtre');
+                                    ('The Old Timers', 'Groupe de rock vintage des années 80'),
+                                    ('Punk Society', 'Groupe de punk rock local'),
+                                    ('The New Waves', 'Groupe d indie rock de Nancy'),
+                                    ('Maxime&Alexandre', 'Groupe de l IUT debutant');
 
 -- Remplissage de la table User
 INSERT INTO User (email, passwd, role) VALUES
-                                           ('jean.dupont@example.com', 'password123', 1),
-                                           ('marie.leclerc@example.com', 'securepass', 2),
-                                           ('paul.durand@example.com', 'mysecretpass', 1);
+                                           ('user1@mail.com', '0a041b9462caa4a31bac3567e0b6e6fd9100787db2ab433d96f6d178cabfce90', 1),
+                                           ('user2@mail.com', '6025d18fe48abd45168528f18a82e265dd98d421a7084aa09f61b341703901a3', 1),
+                                           ('user3@mail.com', '5860faf02b6bc6222ba5aca523560f0e364ccd8b67bee486fe8bf7c01d492ccb', 1),
+                                           ('admin@mail.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 100);
+
 
 -- Remplissage de la table Image
 INSERT INTO Image (filetype, description, data) VALUES
-                                                    ('jpeg', 'Affiche Comedy Night', NULL),
-                                                    ('png', 'Bannière Jazz Live', NULL),
-                                                    ('jpg', 'Affiche Théâtre Classique', NULL);
+                                                    ('jpeg', 'Affiche Soirée Rock Legends', NULL),
+                                                    ('png', 'Bannière Soirée Punk Vibes', NULL),
+                                                    ('jpg', 'Affiche Indie Night', NULL),
+                                                    ('jpg', 'Led rouge', NULL);
 
 -- Remplissage de la table Spectacle2Artiste
 INSERT INTO Spectacle2Artiste (id_spectacle, id_artiste) VALUES
                                                              (1, 1),
                                                              (2, 2),
-                                                             (3, 3);
+                                                             (3, 2),
+                                                             (4, 4),
+                                                             (5, 3);
+
 
 -- Remplissage de la table Spectacle2Image
 INSERT INTO Spectacle2Image (id_spectacle, id_image) VALUES
                                                          (1, 1),
                                                          (2, 2),
-                                                         (3, 3);
+                                                         (3, 2),
+                                                         (4, 4),
+                                                         (5, 3);
