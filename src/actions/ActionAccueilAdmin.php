627595e0ebd2d001f58af10ca586df91ac5b96e3
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-11-08 15:06:50 +0100

    autorisation

--- src/actions/ActionAccueilAdmin.php
+++ src/actions/ActionAccueilAdmin.php
@@ -3,22 +3,33 @@
 namespace Iutnc\Nrv\actions;
 
 use Iutnc\Nrv\auth\AuthProvider;
+use Iutnc\Nrv\auth\Authz;
 use Iutnc\Nrv\exceptions\AuthException;
 
 class ActionAccueilAdmin extends Action
 {
-    private string $form = <<<HTML
-    <form action="?action=accueilAdmin" method="POST">
-        <nav>
-            <a href="?action=afficherSoirees">Afficher les soirées</a>
-            <br>
-
-    </form>
-    HTML;
 
     public function get(): string
     {
-        return $this->form;
+        try {
+            Authz::checkRole(100);
+            return <<<HTML
+    <form action="?action=accueilStaff" method="POST">
+        <nav>
+            <a href="?action=afficherSoirees">Afficher les soirées</a>
+            <br>
+            <a href="?action=accueilStaff">vue staff</a>
+            <br>
+            <a href="?action=accueilUser">vue user</a>
+
+    </form>
+    HTML;
+        } catch (AuthException $e) {
+            return <<<HTML
+                <p>Vous n'avez pas les droits pour accéder à cette page</p>
+                <a href="?action=accueilUser">Retour à la page d'accueil</a>
+            HTML;
+        }
     }
 
     public function post(): string
--- src/actions/ActionAccueilStaff.php
+++ src/actions/ActionAccueilStaff.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace Iutnc\Nrv\actions;
+use Iutnc\Nrv\auth\Authz;
+use Iutnc\Nrv\exceptions\AuthException;
 
 class ActionAccueilStaff extends Action
 {
-    private string $form = <<<HTML
+
+
+    public function get(): string
+    {
+        try {
+            Authz::checkRole(5);
+            $html = <<<HTML
     <form action="?action=accueilStaff" method="POST">
         <nav>
             <a href="?action=afficherSoirees">Afficher les soirées</a>
@@ -12,10 +20,14 @@ class ActionAccueilStaff extends Action
 
     </form>
     HTML;
+            return $html;
+        } catch (AuthException $e) {
+            return <<<HTML
+                <p>Vous n'avez pas les droits pour accéder à cette page</p>
+                <a href="?action=accueilUser">Retour à la page d'accueil</a>
+            HTML;
+        }
 
-    public function get(): string
-    {
-        return $this->form;
     }
 
     public function post(): string
