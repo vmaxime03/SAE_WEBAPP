5ea475b040b6d9268db88373cb3509017ad277c7
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-11-08 09:18:00 +0100

    accueil

--- src/actions/ActionAccueil.php
+++ src/actions/ActionAccueil.php
@@ -21,30 +21,6 @@ class ActionAccueil extends Action
         return $this->form;
     }
 
-    public function post(): string
-    {
-        if (!(isset($_POST['email']) && isset($_POST['passwd'])) ||
-            $_POST['email'] == "" || $_POST['passwd'] == "") {
-            return $this->form . "entrée manquante";
-        }
 
-        try {
-            $user = AuthProvider::getSignedInUser();
-            echo var_dump($user);
-            if ($user->email === $_POST["email"] &&
-                password_verify($_POST['passwd'], $user->passwd)) {
-                return "Vous etes deja connecté <br><a href=''>Accueil</a>";
-            } else {
-                throw new AuthException();
-            }
-        } catch (AuthException $e) {
-            try {
-                $user = AuthProvider::signin($_POST["email"], $_POST["passwd"]);
-                unset($_SESSION['playlist']);
-                return "Rebonjour " . $user->email . "<br><a href=''>Accueil</a>";
-            } catch (AuthException $e) {
-                return $this->form . "erreur lors de la connexion";
-            }
-        }
-    }
+
 }
