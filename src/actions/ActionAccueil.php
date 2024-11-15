17251c0c7e7c111e545604a6cc702ffddabd03e1
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-11-15 15:42:46 +0100

    legere modif

--- src/actions/ActionAccueil.php
+++ src/actions/ActionAccueil.php
@@ -9,6 +9,7 @@ class ActionAccueil extends Action
 {
     public function get(): string
     {
+
         $htmlUser = <<<HTML
         <form action="?action=accueil" method="POST">
             <link rel="stylesheet" href="../style.css">
@@ -76,9 +77,13 @@ HTML;
                     </nav>
 HTML;
                     break;
+                default:
+                    $html = $htmlUser;
+                    break;
             }
         } catch (AuthException $e) {
-            $html = $htmlUser;
+            $html= $htmlUser;
+
         }
 
         return $html;
