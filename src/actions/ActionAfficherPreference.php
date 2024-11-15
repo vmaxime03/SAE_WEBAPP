8bbed53f4cfc0dccdbd4df43c967798ee2345a03
Alexandre014 <alexandre.cosson3@etu.univ-lorraine.fr>
2024-11-15 16:24:37 +0100

    css

--- src/actions/ActionAfficherPreference.php
+++ src/actions/ActionAfficherPreference.php
@@ -9,12 +9,13 @@ class ActionAfficherPreference extends Action
 {
     public function get(): string
     {
+        $html = "<div class='soirees'>";
         if (isset($_SESSION["preferences"])) {
             $preference = unserialize($_SESSION["preferences"]);
 
             if (count($preference) > 0) {
                 $repo = NrvRepository::getInstance();
-                $html = "<div>";
+                $html .= "<div>";
                 foreach ($preference as $key => $value) {
                     $preference[$key] = htmlspecialchars($value);
                     $spectacle = $repo->getSpectableById($value);
