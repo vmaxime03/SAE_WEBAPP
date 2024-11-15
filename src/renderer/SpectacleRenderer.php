d2369a6ece213e99e0723b45bc151f63cab28807
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-11-15 10:15:44 +0100

    afficher tous les spectacle

--- src/renderer/SpectacleRenderer.php
+++ src/renderer/SpectacleRenderer.php
@@ -20,10 +20,9 @@ class SpectacleRenderer implements Renderer
         <div class = 'spectacle'>
             <p>titre : {$this->toRender->titre}</p>
             <p>description : {$this->toRender->description}</p>
-            <p>heure : {$this->toRender->heure}</p>
+            <p>date et heure : {$this->toRender->heure}</p>
             <p>duree : {$this->toRender->duree}</p>
             <p>style : {$this->toRender->style}</p>
-            <p> : {$this->toRender->style}</p>
             <video controls width="250">
                 <source src="{$this->toRender->videoUrl}" type="video/mp4" />
             </video>
