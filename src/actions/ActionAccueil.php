a50d3923f0e4e45a96dabde3915a9095166831c4
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-11-15 10:36:38 +0100

    acces accueil sans etre connecter

--- src/actions/ActionAccueil.php
+++ src/actions/ActionAccueil.php
@@ -7,7 +7,6 @@ use Iutnc\Nrv\exceptions\AuthException;
 
 class ActionAccueil extends Action
 {
-
     public function get(): string
     {
         $html = <<<HTML
@@ -19,29 +18,35 @@ class ActionAccueil extends Action
             <br>
             <a href="?action=afficherSoirees">Afficher les soirées</a>
             <br>
+            <a href="?action=afficherTousSpectacle">Afficher tous les spectacles</a>
+            <br>
         </nav>
     </form>
 HTML;
         $htmlStaff = <<<HTML
             <a href="?action=creerSoiree">créer Soirée</a>
             <br>
-            <a href="?action=creerSoiree">créer Spectacle</a>
+            <a href="?action=creerSpectacle">créer Spectacle</a>
             <br>
-
 HTML;
 
-        $user = AuthProvider::getSignedInUser();
-        switch($user->getRole($user)){
-            case 5:
-                $html .= $htmlStaff;
-                break;
-            case 100:
-                $html .= $htmlStaff;
-                $html .= <<<HTML
-                <a href="?action=signup"> Enregistrer un membre du Staff</a>
+        try {
+            $user = AuthProvider::getSignedInUser();
+            switch($user->getRole($user)){
+                case 5:
+                    $html .= $htmlStaff;
+                    break;
+                case 100:
+                    $html .= $htmlStaff;
+                    $html .= <<<HTML
+                    <a href="?action=signup"> Enregistrer un membre du Staff</a>
 HTML;
-                break;
+                    break;
+            }
+        } catch (AuthException $e) {
+            // User is not connected, show default accueil page
         }
+
         return $html;
     }
 
